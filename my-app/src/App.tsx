43580dd9d14687130e4ee4207f7f5a6b8cafb4c0
Max Devoe <maxwell.devoe@gmail.com>
2022-03-02 08:36:24 -0500

    init commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react"
+import TodoItem from "./components/TodoItem"
+import AddTodo from "./components/AddTodo"
+import {getTodos, addTodo, updateTodo, deleteTodo} from "./API"
+import { isToken } from "typescript"
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = (): void => {
+    getTodos()
+    .then(({ data: {todos}}: ITodo[] | any) => setTodos(todos))
+    .catch((err: Error) => console.log(err))
+  }
+}
